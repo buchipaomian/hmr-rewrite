145887d3a1a1d8274b733f1b35d1815cdb4efdb9
buthipomain <871443204@qq.com>
2019-05-09 10:54:56 +0800

    sorry i forgot

--- models/multiconv.py
+++ models/multiconv.py
@@ -50,3 +50,15 @@ class MutiConv(torch.nn.Module):
             torch.nn.ReLU(),
             torch.nn.Linear(256, 60)
         )
+    def forward(self, x):
+        conv1_out = self.conv1(x)
+        conv2_out = self.conv2(conv1_out)
+        conv3_out = self.conv3(conv2_out)
+        conv4_out = self.conv3(conv3_out)
+        conv5_out = self.conv3(conv4_out)
+        conv6_out = self.conv3(conv5_out)
+        conv7_out = self.conv3(conv6_out)
+        conv8_out = self.conv3(conv7_out)
+        res = conv8_out.view(conv8_out.size(0), -1)
+        out = self.dense(res)
+        return out
